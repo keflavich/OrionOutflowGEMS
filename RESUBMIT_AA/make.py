d98d1708f69e04df816e5c48b432a054ac55e4ec
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-06-29 19:14:39 +0200

    arxiv script and minor tex fixes

--- RESUBMIT_AA/make.py
+++ RESUBMIT_AA/make.py
@@ -4,6 +4,7 @@ import shutil
 import glob
 import argparse
 import os
+import tarfile
 
 parser = argparse.ArgumentParser(description='Make latex files.')
 parser.add_argument('--referee', default=False,
@@ -14,8 +15,52 @@ parser.add_argument('--infile', default='OrionGEMS.tex')
 parser.add_argument('--outfile', default='auto')
 parser.add_argument('--noclean', default=True, action='store_false')
 parser.add_argument('--both', default=False, action='store_true')
+parser.add_argument('--arxiv', default=False, action='store_true')
 
 args = parser.parse_args()
+
+def do_arxiv():
+    filelist = [
+        'Orion.bib',
+        'OrionGEMS.bbl',
+        'OrionGEMS.tex',
+        'Orion_figures1.tex',
+        'Orion_figures2.tex',
+        'Orion_table1.tex',
+        'Orion_table2.tex',
+        'aa.cls',
+        'apj_w_etal.bst',
+        'deluxetable.sty',
+        'fig10_cartoon.pdf',
+        'fig11_Silvia_model_on_GEMS2.pdf',
+        'fig12_density_100yrs.png',
+        'fig13_x_velocity_100yrs.pdf',
+        'fig14_y_velocity_100yrs.pdf',
+        'fig15_z_velocity_100yrs.pdf',
+        'fig16_temperature_100yrs.png',
+        'fig17_jetstar.pdf',
+        'fig18_diskshadow.pdf',
+        'fig1_H2_FeII_GSAOI.pdf',
+        'fig2.pdf',
+        'fig3_fingers_on_medianH2.pdf',
+        'fig4_H2_FeII_Ks_color.pdf',
+        'fig5_H2_2013_1999.pdf',
+        'fig6_H2_PMs_fingers.pdf',
+        'fig7_FeII_PMs_fingers.pdf',
+        'fig8_H2_HVCC.pdf',
+        'fig9_FeII_HVCC.pdf',
+    ]
+    if not os.path.isdir('arxiv'):
+        os.mkdir('arxiv')
+    tf = tarfile.TarFile("arxiv.tar.gz", mode="w")
+    for fn in filelist:
+        if 'pdf' in fn:
+            os.system('gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=arxiv/{0} {0}'.format(fn))
+            tf.add('arxiv/{0}'.format(fn), arcname=fn)
+        else:
+            tf.add(fn)
+    tf.close()
+    shutil.rmtree('arxiv')
 
 
 def do_everything():
@@ -72,7 +117,9 @@ def do_everything():
 
     subprocess.call(gscmd)
 
-if args.both:
+if args.arxiv:
+    do_arxiv()
+elif args.both:
     args.referee = True
     do_everything()
     args.referee = False
